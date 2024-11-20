3687655869241f3d0db1bd96e7033d6374a14056
Illia <doku_isv@icloud.com>
2024-11-20 11:43:46 +0100

    feat(reservation): add reservation contr and serv

--- routes/user_routes.go
+++ routes/user_routes.go
@@ -7,11 +7,16 @@ import (
 )
 
 // RegisterUserRoutes defines user-specific routes
-func RegisterUserRoutes(r *gin.Engine, userController *controllers.UserController) {
+func RegisterUserRoutes(r *gin.Engine, userController *controllers.UserController, reservationController *controllers.ReservationController) {
 	user := r.Group("/")
 	user.Use(middleware.JWTMiddleware())
 	{
+		// User routes
 		user.GET("/users/:id", userController.GetUserDetails)
 		user.PUT("/users/:id", userController.UpdateUserDetails)
+
+		// Reservation routes
+		user.GET("/reservation/user", reservationController.GetUserReservationsHandler)
+		user.GET("/reservation/:id", reservationController.GetReservationByIDHandler)
 	}
 }
